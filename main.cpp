06b99e423a78acc4ad2db000857b7846f5b06c1e
airhammerandy88 <ajone355@wgu.edu>
2025-12-30 19:27:23 +0000

    creates load loops for doubles and strings

--- main.cpp
+++ main.cpp
@@ -6,63 +6,38 @@ int main(){
   double userIn1 = 0;
   int expressNum = 0;
   
-  double<vector> userInputArray;
-  std::string<vector> mathTypeArray;
-  double<vector> expressionArray;
+  std::vector <double> userInputArray;
+  std::vector <std::string> mathTypeArray;
+  std::vector <double> expressionArray;
   std::string mathType;
   //std::string negate;
   double answerOut = 0;
   //bool unsummon = false;
   
   //while(unsummon == false){
-  std::cout << "Input math expression, with 2 numbers and, -, +, * /" << "example: 3 + 4" << std::endl;
+  std::cout << "Input math expression, with numbers and, -, +, * /" << "example: 3 + 4" << std::endl;
   
-  std::cin >> expressNum
+  std::cin >> expressNum;
   
+
   for(int i = 0; i < expressNum; ++i){ //loads userinput numbers
-  std::cin >> userIn1;
-  userInputArray.push_back(i);
+    std::cin >> userIn1;
+    userInputArray.push_back(userIn1);
   }
   
+
+
   for(int i = 0; i < expressNum; ++i){ //loads mathtype strings
-  std::cin >> mathType;
-  if (mathType != "-" || mathType != "+" || mathType !="*" || mathType !="/"){
+    std::cin >> mathType;
+  while (mathType != "-" || mathType != "+" || mathType != "*" || mathType != "/")
+  {
     std::cout << "Invaild input " << std:: endl;
     std::cin >> mathType;
   }
-  mathTypeArray.push_back(i);
+ 
+  mathTypeArray.push_back(mathType);
   }
-  
-  for(int = i; i < expressNum; ++i){
-  
-  mathTypeArray.at(i);
-  if(mathType.at(i) == "-"){
-    expressionArray.push_back(i) = -;
-  }
-  else if(mathType.at(i) == "+"){
-    expressionArray.push_back(i) = +;
-  }
-  else if(mathType == "*"){
-    expressionArray.push_back(i) = *;
-  }
-  else if(mathType == "/"){
-    expressionArray.push_back(i) = /;
-  }
-  else {
-    expressionArray.push_back(userInputArray.at(i));
-  }
-  }
-  
-  std::cout << userIn1 << " " << mathType << " " << userIn2  << " = " << answerOut << std::endl;
-  //std::cout << "to end program type the word off" << std::endl;
- //std::cin >> negate;
-  
-  //if(negate == "off" || negate == "Off" || negate == "OFF"){
-    //unsummon = true;
-  //}
-  //else {
-    //std::cout << "Input new expression to be solved" << std::endl;
-  //}
-  
-  //}  
+
+  for()
+
 }
