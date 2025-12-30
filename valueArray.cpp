398f1744a2e8098d079424c9ae4f15393561b41d
airhammerandy88 <ajone355@wgu.edu>
2025-12-30 15:03:04 +0000

    creates valarr cpp

---
+++ valueArray.cpp
@@ -0,0 +1,36 @@
+#include "valueArray.h"
+
+double decValue::GetUserValue()
+{
+    return userValue;
+}
+
+int decValue::GetCounter()
+{
+    return counter;
+}
+
+double decValue::GetValueArray()
+{
+    std::cin >> counter;
+    for(int i = 0; i < counter;++i){
+    return valueArray.at(i);
+    std::cout << " " << std::endl;
+    }
+}
+
+void decValue::SetUserValue(double uVal)
+{
+}
+
+void decValue::LoadValArr(double valArr)
+{
+}
+
+void decValue::Print()
+{
+}
+
+void decValue::SetCounter(int count)
+{
+}
