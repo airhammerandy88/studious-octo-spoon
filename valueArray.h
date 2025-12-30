c05ccaccfdf6807d2bdcf4b1b0cd055da5b01d85
airhammerandy88 <ajone355@wgu.edu>
2025-12-30 14:47:53 +0000

    creates valueArray.h class

---
+++ valueArray.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class decValue{
+    private:
+        double userValue = 0;
+        int counter = 0;
+        double<vector> valueArray;
+    public:
+        double GetUserValue();
+        int GetCounter();
+        double GetValueArray();
+
+        void SetUserValue(double uVal);
+        void SetCounter(int count);
+        void SetUserValue(double valArr);
+
+    }
